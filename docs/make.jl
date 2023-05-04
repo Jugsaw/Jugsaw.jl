ee910035f905e66e0e24b5b522e17105cbc7e347
GiggleLiu <cacate0129@gmail.com>
2023-05-05 00:04:31 +0800

    update

--- docs/make.jl
+++ docs/make.jl
@@ -2,35 +2,39 @@ using Jugsaw
 using JugsawIR
 using Documenter, DocumenterMarkdown
 
-_format = length(ARGS) >= 1 ? ARGS[1] : "HTML"
-format = if _format == "HTML"
-    Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://Jugsaw.github.io/Jugsaw.jl",
-        edit_link="main",
-        assets=String[],
+formats = length(ARGS) >= 1 ? ARGS[1:end] : ["HTML"]
+allowed_formats = ["HTML", "Markdown"]
+any(∉(allowed_formats), formats) && error("documentation format error, expected $allowed_formats, but got: $formats")
+
+for fmt in formats
+    format, build = if fmt == "HTML"
+        Documenter.HTML(;
+            prettyurls=get(ENV, "CI", "false") == "true",
+            canonical="https://Jugsaw.github.io/Jugsaw.jl",
+            edit_link="main",
+            assets=String[],
+        ), "build"
+    else
+        DocumenterMarkdown.Markdown(), "build-markdown"
+    end
+
+    @info "generating documents of format: $format"
+
+    makedocs(;
+        build=joinpath(@__DIR__, folder),
+        modules=[Jugsaw, JugsawIR],
+        authors="Jugsaw Computing Inc.",
+        repo="https://github.com/Jugsaw/Jugsaw.jl/blob/{commit}{path}#{line}",
+        sitename = "Documentation | Jugsaw",
+        format = format,
+        pages=[
+            "Home" => "index.md",
+            "Get Started" => "get-started.md",
+            "Clients" => ["client.md"],
+            "Jugsaw Developer" => ["man/Jugsaw.md", "man/JugsawIR.md", "design.md"],
+        ],
     )
-elseif _format == "Markdown"
-    DocumenterMarkdown.Markdown()
-else
-    error("documentation format error, got: $_format")
 end
-
-@info "generating documents of format: $format"
-
-makedocs(;
-    modules=[Jugsaw, JugsawIR],
-    authors="Jugsaw Computing Inc.",
-    repo="https://github.com/Jugsaw/Jugsaw.jl/blob/{commit}{path}#{line}",
-    sitename = "Documentation | Jugsaw",
-    format = format,
-    pages=[
-        "Home" => "index.md",
-        "Get Started" => "get-started.md",
-        "Clients" => ["client.md"],
-        "Jugsaw Developer" => ["man/Jugsaw.md", "man/JugsawIR.md", "design.md"],
-    ],
-)
 
 deploydocs(;
     repo="github.com/Jugsaw/Jugsaw.jl",
