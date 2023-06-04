9685ab11f073dd70bd5a074ade7f23642cc83793
GiggleLiu <cacate0129@gmail.com>
2023-06-05 02:21:58 +0800

    update

--- example/hello_world/app/app.jl
+++ example/hello_world/app/app.jl
@@ -7,10 +7,6 @@ A function returns "Hello, \$x".
 """
 greet(x::String) = "Hello, $x"
 
-function generate_app()
-    app = AppSpecification(:helloworld)
-    @register app greet("Jinguo") == "Hello, Jinguo"
-    return app
-end
+app = AppSpecification(:helloworld)
+@register app greet("Jinguo") == "Hello, Jinguo"
 # Jugsaw.Server.serve(r; localmode=false, host="127.0.0.1")
-app = generate_app()
