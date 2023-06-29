7eafac1bbf22694ba32573931edf9fb19f2aa099
GiggleLiu <cacate0129@gmail.com>
2023-06-29 20:48:02 +0800

    fix codecov

--- .ci/codecov.jl
+++ .ci/codecov.jl
@@ -16,12 +16,17 @@ function main()
     root_directory = dirname(@__DIR__)
 
     if length(ARGS) == 0
-        package_names = readdir(joinpath(root_directory, "src", "jl"))
+        package_names = readdir(joinpath(root_directory, "lib"))
+        push!(package_names, "Jugsaw")
     else
         package_names = ARGS
     end
     dirs = map(package_names) do name
-        joinpath(root_directory, "src", "jl", name, "src")
+        return if name == "Jugsaw"
+            joinpath(root_directory, "src")
+        else
+            joinpath(root_directory, "lib", name, "src")
+        end
     end
     pfs = mapreduce(process_folder, vcat, dirs)
     LCOV.writefile(joinpath(root_directory, "lcov.info"), pfs)
